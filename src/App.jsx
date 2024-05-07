fc8a5efa7338e8b36d5755d962bad6e88561613e
JonathasNJohnny <jonathasnobrega59@gmail.com>
2024-05-07 01:33:12 -0300

    Api connect and test - Pode Apagar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  //api ping test -- Start
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/ping');
+        if (response.ok) {
+          const jsonData = await response.json();
+          setData(jsonData);
+        } else {
+          console.error('Failed to fetch data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+  //api ping test -- Start
 
 
   return (
     <>
-
-    <h1>hi
-    </h1>
-      
+      <h1>hi</h1>
+      {data && <p>Response from API: {data.message}</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
