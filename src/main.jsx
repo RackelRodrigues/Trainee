540d07b5129557805023eeb8217691786bb3d9b1
JonathasNJohnny <jonathasnobrega59@gmail.com>
2024-05-08 16:58:25 -0300

    Mudanças que Rackel fez

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import App from './App.jsx';
+import { createRoot } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login.jsx';
+import Createaccount from './pages/UserCreateAccount/Createaccount.jsx';
+import ErroPage from './pages/Erropage/Erropage.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')).render(
+
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/CriarConta" element={<Createaccount />} />
+          <Route path="*" element={<ErroPage error={{ message: 'Página não encontrada' }} />} />
+       
+         
+        </Routes>
+      </Router>
+    </React.StrictMode>
+
+);
---
+++ src/pages/Erropage/Erropage.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+color: #fff;
+
+`;
+
+const ErroPage = ()=>{
+    return(
+        <>
+        <H1>404</H1>
+        </>
+    )
+}
+
+
+export default ErroPage;
